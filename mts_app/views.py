cf971214f33b349c3624cf822102800b5ae4714a
conber1 <48555903+conber1@users.noreply.github.com>
2020-09-17 15:21:57 +0300

    Update views.py

--- mts_app/views.py
+++ mts_app/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import redirect
 import roundrobin
 from urllib.parse import urlparse
 
+
 SERVERS = ["ORIGIN", "CDN_A", "CDN_B"] #list of the nodes
 
 get_roundrobin = roundrobin.basic(SERVERS) 
+
 
 def redirect_to_server(request): #function which redirect us to new server depending on previous server
     new_state = get_roundrobin()
